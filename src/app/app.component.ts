f81750623f8d77c33dc85e12424b7600e3e328c4
benjamin <benjamin.d@make-it.fr>
2023-10-20 10:39:23 +0200

    add detail and list components with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,6 @@ import { POKEMONS } from './mock-pokemon-list';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styles: []
 })
 
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList)
-  }
-
-  selectPokemon(pokemonId: number) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(p => p.id === pokemonId);
-
-    if (pokemon) {
-      console.log(`Vous avez demandé ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Le pokemon ${pokemonId} n'existe pas`);
-    }
-  }
-}
+export class AppComponent {}
